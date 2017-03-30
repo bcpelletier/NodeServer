79eb3ef0c386b1374f1c652ec5dceddd015aa06a
Boris Carlos Pelletier Gómez <bcpelletier@outlook.com>
2017-03-31 00:25:04 +0200

    V1 Node server

--- NodeServer/app.js
+++ NodeServer/app.js
@@ -1,3 +1,136 @@
-'use strict';
+var net = require('net');
+var sockets = [];
+
+var svr = net.createServer(function (sock) {
+    console.log('Connected: ' + sock.remoteAddress + ':' + sock.remotePort);
+    sockets.push(sock);
+
+    sock.write('Welcome to the server!\n');
+
+    sock.on('data', function (data) {
+        console.log(data);
+
+        for (var i = 0; i < sockets.length; i++) {
+            if (sockets[i] != sock) {
+                if (sockets[i]) {
+                    sockets[i].write(data);
+                }
+            }
+        }
+        sock.write('Hello, server! Love, Client.');
+    });
+
+    
+
+
+    sock.on('end', function () {
+        console.log('Disconnected: ' + sock.remoteAddress + ':' + sock.remotePort);
+        var idx = sockets.indexOf(sock);
+        if (idx != -1) {
+            delete sockets[idx];
+        }
+    });
+});
+
+var svraddr = '127.0.0.1';
+var svrport = 1234;
+
+svr.listen(svrport, svraddr);
+console.log('Server Created at ' + svraddr + ':' + svrport + '\n');
+
+
+
+
+
+
+
+/*'use strict';
 
 console.log('Hello world');
+
+var express = require('express')
+
+var app = express()
+
+app.get('/notes', function (req, res) {
+    res.json({ notes: "This is your notebook. Edit this to start saving your notes!" })
+})
+
+app.listen(3000)
+
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+//var io = require('../..')(server);
+var port = process.env.PORT || 3000;
+var io = require('socket.io').listen(server);
+
+server.listen(port, function () {
+    console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+// Chatroom
+
+var numUsers = 0;
+
+io.on('connected', function (socket) {
+    var addedUser = false;
+
+    // when the client emits 'new message', this listens and executes
+    socket.on('new message', function (data) {
+        // we tell the client to execute 'new message'
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
+    });
+
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', function (username) {
+        if (addedUser) return;
+
+        // we store the username in the socket session for this client
+        socket.username = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
+
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', function () {
+        socket.broadcast.emit('typing', {
+            username: socket.username
+        });
+    });
+
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', function () {
+        socket.broadcast.emit('stop typing', {
+            username: socket.username
+        });
+    });
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function () {
+        if (addedUser) {
+            --numUsers;
+
+            // echo globally that this client has left
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    });
+});*/
